d29fe02b6b5a9388b8f974f3dab41c90580525fc
felnarg <andres.f.93@hotmail.com>
2023-12-28 16:25:04 -0500

    add migrations

---
+++ Crud/Migrations/202312282113394_InitialMigration.cs
@@ -0,0 +1,28 @@
+﻿namespace Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CreditCards",
+                c => new
+                    {
+                        CardNumber = c.String(nullable: false, maxLength: 16),
+                        Id = c.Int(nullable: false),
+                        Holder = c.String(nullable: false),
+                        Balance = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.CardNumber);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.CreditCards");
+        }
+    }
+}
