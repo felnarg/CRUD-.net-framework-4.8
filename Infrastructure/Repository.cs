f9670e756fb426f48259aa0bd7fb3d14154078a0
felnarg <andres.f.93@hotmail.com>
2023-12-29 09:58:06 -0500

    IRepository en marcha

---
+++ Infrastructure/Repository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Domain.Models;
+using NPOI.OpenXmlFormats.Spreadsheet;
+using NPOI.SS.Formula.Functions;
+
+namespace Infrastructure
+{
+    public class Repository : IRepository<T> 
+    {
+        private readonly CreditCardDbContext _context;
+
+        public Repository(CreditCardDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(Guid id)
+        {
+            T entity = _context.Set<T>().Find(id) ?? throw new Exception("No se encontró el Id especificado");
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(Guid id)
+        {
+            T entity = _context.Set<T>().Find(id);
+            return entity ?? throw new Exception("No se encontró el Id especificado");
+        }
+
+        public void SaveEntity(T entity)
+        {
+            _context.Set<T>().Add(entity); 
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            if(entity.ToString().Any())
+            {
+                _context.Set<T>().AddOrUpdate(entity);
+                _context.SaveChanges();
+            }
+            else            
+                throw new Exception("No se encontró el Id especificado");
+            
+        }
+    }
+}
