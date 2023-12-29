ad6f41c55592d04fe7857a85d4772b43f676605a
felnarg <andres.f.93@hotmail.com>
2023-12-29 09:01:48 -0500

    xx

--- Infrastructure/IRepository.cs
+++ Infrastructure/IRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application
+namespace Infrastructure
 {
-    public class Class1
+    internal class IRepository
     {
     }
 }
