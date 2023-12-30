9add021f8a51f886a77937e0f72a9fdf0d1d223c
felnarg <andres.f.93@hotmail.com>
2023-12-30 13:54:49 -0500

    inyeccion de dependencias y resolver agregados

--- Crud/Controllers/HomeController.cs
+++ Crud/Controllers/HomeController.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Infrastructure;
 
 namespace Crud.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly CreditCardDbContext _dbcontext;
+
+        public HomeController(CreditCardDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+        public ActionResult DbConnection()
+        {
+            _dbcontext.Database.CreateIfNotExists();
+            return Content($"Base de datos: {_dbcontext.Database.Connection.Database}");
         }
     }
 }
